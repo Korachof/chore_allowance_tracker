a5b7ae4706716e01cb3d388a90424760573c2bb7
Christopher Partin <christopherpartin11@gmail.com>
2024-08-14 11:01:28 -0700

    refactored main.py a bit to spread the information from the script between various functions.

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import sys
 
 def main():
     """main script for Chore Allowance Tracker"""
+    start_menu()
+    menu_select()
+
+
+def start_menu():
+    """start menu for main script"""
     print("Welcome to the Chore Allowance Tracker!\n")
     print("Please Select from the Options Below\n")
     print("1) New User")
     print("2) Load User")
     print("3) Exit Program\n")
+
+
+def menu_select():
+    """menu select functionality for main script"""
     user_input = None
     while user_input != "1" and user_input != "2" and user_input != "3":
         user_input = input("Type the Associated Number and Press ENTER: ")
@@ -20,24 +30,26 @@ def main():
         load_user()
 
     elif user_input == "3":
-        print("Are you sure you want to EXIT?\n")
-        exit_check = input("Type 1 for YES. Type Any Other Key For NO: ")
-        if exit_check == "1":
-            sys.exit()
-
-        else:
-            main()
+        exit_program()
 
 
 def create_user():
     pass
 
-def show_user_list():
-    pass
-
 def load_user():
     pass
 
+def exit_program():
+    print("Are you sure you want to EXIT?\n")
+    exit_check = input("Type 1 for YES. Type Any Other Key For NO: ")
+    if exit_check == "1":
+        sys.exit()
+    else:
+        print("")       # an empty print to skip a line visually
+        main()
+
+def show_user_list():
+    pass
 
 if __name__ == "__main__":
     main()
