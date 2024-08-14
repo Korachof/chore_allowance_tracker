850d734bf9ecd6984316c508ab6a577cba00f2d3
Christopher Partin <christopherpartin11@gmail.com>
2024-08-14 09:51:58 -0700

    Added inherited classes for chores. Also moved "deadline" to OneOffGoal instead of Chore, since OnGoingChores wont have a deadline.

--- chores.py
+++ chores.py
@@ -1,8 +1,7 @@
 class Chore:
-    def __init__(self, name, description, deadline, cash_amount):
+    def __init__(self, name, description, cash_amount):
         self._name = name
         self._description = description
-        self._deadline = deadline
         self._cash_amount = cash_amount
 
     def get_name(self):
@@ -12,10 +11,6 @@ class Chore:
     def get_description(self):
         """returns description (STR)"""
         return self._description
-    
-    def get_deadline(self):
-        """returns deadline (STR)"""
-        return self._deadline
     
     def get_cash_amount(self):
         """returns cash amount (FLOAT)"""
@@ -33,19 +28,47 @@ class Chore:
         self._description = new_description
         return self._description
 
-    def set_deadline(self, new_deadline):
-        """sets/edits deadline (STR)
-        returns: UPDATED self._deadline (STR)"""
-        self._deadline = new_deadline
-        return self._deadline
-
     def set_cash_amount(self, new_cash_amount):
         """sets/edits self._cash_amount (FLOAT)
         returns: UPDATED self._cash_amount (FLOAT)"""
         self._cash_amount = new_cash_amount
         return self._cash_amount
     
+
+class OneOffGoal(Chore):
+    def __init__(self, name, description, cash_amount, deadline):
+        super().__init__(name, description, cash_amount)
+        self._deadline = deadline
+
+    def get_deadline(self):
+        """returns deadline (STR)"""
+        return self._deadline
+
+    def set_deadline(self, new_deadline):
+        """sets/edits deadline (STR)
+        returns: UPDATED self._deadline (STR)"""
+        self._deadline = new_deadline
+        return self._deadline
+
+
+class OngoingChore(Chore):
+    def __init__(self, name, description, cash_amount, completed_count):
+        super().__init__(name, description, cash_amount)
+        self._completed_count = completed_count
+
+    def get_completed_count(self):
+        """returns completed_count (INT)"""
+        return self._completed_count
     
+    def incr_count(self):
+        """increments completed_count (INT) by 1
+        returns self._completed_count"""
+        self._completed_count += 1
+        return self._completed_count
+
+
+    
+
         
 
 
